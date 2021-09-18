24b006e6e2d5afb50c81d67558fe1a5ce9a405de
Cerubois <55885923+Cerubois@users.noreply.github.com>
2021-09-18 14:59:58 -0400

    September 18 Class

---
+++ Scripts/TestScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+
+	public int bob = 10;
+	private int joe = 5;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		print("Hello");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		print(bob);
+	}
+}
