0fbd3170aa0f7f830fc70f102e7bf7df9efac82f
Cerubois <55885923+Cerubois@users.noreply.github.com>
2022-04-09 15:16:03 -0400

    April 9 Class
    - Fixed up an Enemy bug
    - Added Player detection to Enemy

--- Scripts/EnemyAI.cs
+++ Scripts/EnemyAI.cs
@@ -29,11 +29,15 @@ public class EnemyAI : MonoBehaviour
 		patrolPoint0 = transform.position;
 		patrolPoint1 = transform.position + transform.forward * 10;
 
+		playerObject = GameObject.FindGameObjectWithTag("Player");
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+		playerPos = playerObject.transform.position;
 
 		switch (curMode) {
 
@@ -41,8 +45,17 @@ public class EnemyAI : MonoBehaviour
 
 				Patrol();
 
+				if (Vector3.Distance(transform.position, playerPos) < 20)
+					curMode = behaveMode.attack;
+
 			break;
 
+			case behaveMode.attack:
+
+				myRigidbody.position = Vector3.MoveTowards(myRigidbody.position, playerPos, moveSpeed * Time.deltaTime);
+				transform.LookAt(playerPos);
+
+				break;
 
 
 
@@ -55,16 +68,18 @@ public class EnemyAI : MonoBehaviour
 
 		if(curPatrol == 0) {
 
-			myRigidbody.position += Vector3.MoveTowards(myRigidbody.position, patrolPoint0, moveSpeed * Time.deltaTime);
+			myRigidbody.position = Vector3.MoveTowards(myRigidbody.position, patrolPoint0, moveSpeed * Time.deltaTime);
+			transform.LookAt(patrolPoint0);
 
-			if(Vector3.Distance(myRigidbody.position, patrolPoint0) < 1) {
+			if (Vector3.Distance(myRigidbody.position, patrolPoint0) < 1) {
 				curPatrol = 1;
 			}
 
 		}
 		if (curPatrol == 1) {
 
-			myRigidbody.position += Vector3.MoveTowards(myRigidbody.position, patrolPoint1, moveSpeed * Time.deltaTime);
+			myRigidbody.position = Vector3.MoveTowards(myRigidbody.position, patrolPoint1, moveSpeed * Time.deltaTime);
+			transform.LookAt(patrolPoint1);
 
 			if (Vector3.Distance(myRigidbody.position, patrolPoint1) < 1) {
 				curPatrol = 0;
